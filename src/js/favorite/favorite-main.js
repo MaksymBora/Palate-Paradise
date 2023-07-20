78cfe5429776a243e00cf5d3a8f15749f975b50e
Maksym <maks@iMac-Maksym.fritz.box>
2023-07-21 01:41:19 +0200

    add fav-card fav.html

--- src/js/favorite/favorite-main.js
+++ src/js/favorite/favorite-main.js
@@ -24,6 +24,9 @@ recipeApiSerive.getRecipe().then(response => {
 
 function renderingFavRec() {
   const favList = document.querySelector('.fav-list');
+  const empty = document.querySelector('.fav-empty');
+  const heart = document.querySelector('.hear-icon');
+  const hero = document.querySelector('.fav-hero');
 
   const arrData = localStorage.getItem('favotires-data');
   const data = JSON.parse(arrData);
@@ -31,9 +34,38 @@ function renderingFavRec() {
 
   const { _id, title, category, rating, preview, description } = data[0];
 
-  const markup = `<div>${_id}</div>`;
+  const markup = `
+   <div class="rec-item" style="background: linear-gradient(0deg, rgba(5, 5, 5, 0.6),
+                      rgba(5, 5, 5, 0)),
+                      url(${preview}); 
+                      background-position: center;
+                      background-size: cover;">
+        <div class="upper-part">
+          
+          <button class="heart-btn" type="button">
+            <svg class="icon-heart" width="18" height="16">
+              <use href="./images/sprite.svg#heart"></use>
+            </svg>
+          </button>
 
+          <h2 class="rec-card-title">BANANA PANCAKES</h2>
+          <p class="rec-card-desc">
+            Banana pancakes are a fluffy and sweet breakfast dish made with
+            mashed ripe bananas, eggs, flour, and a touch of cinnamon, cooked to
+            perfection on a
+          </p>
+
+          <div class="rec-rate">
+            <p class="rate">5</p>
+            <button type="button" class="rec-btn-open">See recipe</button>
+          </div>
+        </div>
+      </div>`;
+
+  // empty.classList.add('visually-hidden');
+  hero.classList.remove('visually-hidden');
   favList.insertAdjacentHTML('beforeend', markup);
+  heart.classList.add('.active');
 }
 
 renderingFavRec();
