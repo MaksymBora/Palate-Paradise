3d1198cdcc2c02b05993fbc15d1e81ec96db71ec
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-07-21 08:57:46 +0200

    add css classes

--- src/js/hero.js
+++ src/js/hero.js
@@ -25,24 +25,29 @@ function displayEvents(eventsData) {
     chefImage.src = event.cook.imgUrl;
     chefImage.alt = event.cook.name;
     eventCard.appendChild(chefImage);
+    chefImage.classList.add('chef-img');
 
     const dishPreviewImage = document.createElement('img');
     dishPreviewImage.src = event.topic.previewUrl;
     dishPreviewImage.alt = event.topic.name;
     eventCard.appendChild(dishPreviewImage);
+    dishPreviewImage.classList.add('dishPreview-img');
 
     const eventName = document.createElement('h2');
     eventName.textContent = event.topic.name;
     eventCard.appendChild(eventName);
+    eventName.classList.add('event-name');
 
     const eventRegion = document.createElement('p');
     eventRegion.textContent = event.topic.area;
     eventCard.appendChild(eventRegion);
+    eventRegion.classList.add('event-region');
 
     const dishImage = document.createElement('img');
     dishImage.src = event.topic.imgUrl;
     dishImage.alt = event.topic.name;
     eventCard.appendChild(dishImage);
+    dishImage.classList.add('dish-img');
 
     eventsContainer.appendChild(eventCard);
   });
