ae67c5da4a542a2c40a99421a00db3fd51ad0147
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-07-23 00:14:57 +0200

    адаптив

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,4 +1,7 @@
 import Swiper from 'swiper';
+import '/node_modules/swiper/swiper-bundle.js';
+
+import 'swiper/swiper-bundle.css';
 
 fetch('https://tasty-treats-backend.p.goit.global/api/events')
   .then(response => {
@@ -12,15 +15,28 @@ fetch('https://tasty-treats-backend.p.goit.global/api/events')
 
     const swiper = new Swiper('.swiper-container', {
       effect: 'slide',
-      direction: 'horizontal',
+      simulateTouch: true,
       grabCursor: true,
       loop: true,
+      slideToClickedSlide: true,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+      },
+      autoplay: {
+        delay: 1000,
+      },
+      slidesPerView: 0.5,
+
+      speed: 700,
+
       pagination: {
         el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets',
       },
     });
   })
-
   .catch(error => {
     console.error('Error fetching data:', error);
   });
@@ -31,46 +47,25 @@ function displayEvents(eventsData) {
   eventsContainer.innerHTML = '';
 
   eventsData.forEach(event => {
-    const eventCard = document.createElement('div');
-    eventCard.classList.add('swiper-slide');
+    eventsContainer.innerHTML += `
+      <div class="swiper-slide">
+      <div class="element-wrapper">
+    <div class="schef-image-wrapper">
+        <img src="${event.cook.imgUrl}" alt="${event.cook.name}">
+    </div>
+    
+    <div class="dish-Preview-image-wrapper">
+        <img src="${event.topic.previewUrl}" alt="${event.topic.name}">
+        <p class="event-name">${event.topic.name}</p>
+        <p class="event-region">${event.topic.area}</p>
+    </div>
 
-    const chefImageWrapper = document.createElement('div');
-    chefImageWrapper.classList.add('schef-image-wrapper');
-    const chefImage = document.createElement('img');
-    chefImage.src = event.cook.imgUrl;
-    chefImage.alt = event.cook.name;
-    chefImage.classList.add('chef-img');
-    chefImageWrapper.appendChild(chefImage);
-    eventCard.appendChild(chefImageWrapper);
+    <div class="dish-image-wrapper">
+        <img src="${event.topic.imgUrl}" alt="${event.topic.name}">
+    </div>
 
-    const dishPreviewImageWrapper = document.createElement('div');
-    dishPreviewImageWrapper.classList.add('dish-Preview-image-wrapper');
-    const dishPreviewImage = document.createElement('img');
-    dishPreviewImage.src = event.topic.previewUrl;
-    dishPreviewImage.alt = event.topic.name;
-    dishPreviewImageWrapper.appendChild(dishPreviewImage);
-    dishPreviewImage.classList.add('dishPreview-img');
-
-    const eventName = document.createElement('p');
-    eventName.textContent = event.topic.name;
-    dishPreviewImageWrapper.appendChild(eventName);
-    eventName.classList.add('event-name');
-
-    const eventRegion = document.createElement('p');
-    eventRegion.textContent = event.topic.area;
-    dishPreviewImageWrapper.appendChild(eventRegion);
-    eventRegion.classList.add('event-region');
-    eventCard.appendChild(dishPreviewImageWrapper);
-
-    const dishImageWrapper = document.createElement('div');
-    dishImageWrapper.classList.add('dish-image-wrapper');
-    const dishImage = document.createElement('img');
-    dishImage.src = event.topic.imgUrl;
-    dishImage.alt = event.topic.name;
-    dishImage.classList.add('dish-img');
-    dishImageWrapper.appendChild(dishImage);
-    eventCard.appendChild(dishImageWrapper);
-
-    eventsContainer.appendChild(eventCard);
+    </div>
+</div>
+      `;
   });
 }
