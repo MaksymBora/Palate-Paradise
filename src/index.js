9aa500758d37ec6937f7ae06278f065a275630ae
MaksymBora <maxboraod@gmail.com>
2023-07-23 21:53:02 +0200

    fix package

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import './js/service/service-api';
 import './js/mobile-menu';
-import './js/popular';
-
-import './js/all-cat';
-
-
-import './js/mobile-menu';
-
 import './js/hero';
-
-
+import './js/all-cat';
+import './js/popular';
