ac30acbb031b81935447cee0bddea25dab4d1532
MaksymBora <maxboraod@gmail.com>
2023-07-23 18:14:57 +0200

    test

--- src/js/favorite/localStorageUtils.js
+++
@@ -1,8 +0,0 @@
-export function saveToLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
-
-export function getFromLocalStorage(key) {
-  const data = localStorage.getItem(key);
-  return JSON.parse(data);
-}
