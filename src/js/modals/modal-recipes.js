f1d58d496952d92abe3f75f86a3a671b18bc1e34
YuriyBeregovoy <rik0681@gmail.com>
2023-07-23 21:05:48 +0300

    fixed bugs

--- src/js/modals/modal-recipes.js
+++ src/js/modals/modal-recipes.js
@@ -1,1 +1,37 @@
 
+// Отримуємо зображення з API
+
+
+// Рендеримо розмітку та відображаємо дані з API
+
+// const html = ` <div class="recipes-iframe-wrapper">
+//       <iframe
+//         class="recipes-iframe-video"
+//         src="https://www.youtube.com/embed/${}"
+//         frameborder="0"
+//         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+//         allowfullscreen
+//       ></iframe>
+//     </div>
+
+//     <h2 class="recipes-title">${}</h2>
+
+//     <div class="rating-time-container">
+//       <p class="ratinng-value">${}</p>
+     
+//       <p class="recipes-cooking-time">${}</p>
+//     </div>
+
+//     <ul class="recipes-components-list">
+//       <li class="recipes-components-item">
+//         <p class="recipes-components-item_name">${}</p>
+//         <p class="recipes-components-item_quantity">${}</p>
+//       </li>
+//       </ul>
+//     <ul class="recipes-hashtags-list">
+//       <li class="recipes-hashtags-item">${}</li>
+     
+//     </ul>
+
+//     <p class="recipes-description">${}
+//     </p>`
