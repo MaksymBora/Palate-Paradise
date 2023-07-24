c5b7c7a1614b5e34ddf576ff8e77584214f9b083
YuriyBeregovoy <rik0681@gmail.com>
2023-07-24 18:57:08 +0300

    added js functions of the modal window

--- src/js/modals/modal-recipes.js
+++ src/js/modals/modal-recipes.js
@@ -1,56 +1,134 @@
-
 import axios from 'axios';
 
+// const url = 'https://tasty-treats-backend.p.goit.global/api/recipes';
+// // Отримуємо дані рецептів з API
+// function fetchRecipes() {
+//   return axios.get(url)
+//     .then(response => response.data)
+//     .catch(error => {
+//       console.log(error);
+//     });
+// }
 
-// Отримуємо зображення з API
-async function fetchReceipt() {
-  const url = 'https://tasty-treats-backend.p.goit.global/api/recipes';
+// // Дані рецептів, коли вони будуть отримані з API
+// fetchRecipes().then(recipes => {
+//   console.log(recipes);
+// }).catch(error => {
+//   console.log(error);
+// });
 
-  try {
-    const response = await axios.get(url);
-    return response.data; // Повертаємо дані з API
-  } catch (error) {
-    console.error('Помилка при отриманні даних з API:', error);
-    return null;
+// Отримую дані про конкретний рецепт з API по ID
+function fetchRecipe(recipeId) {
+  const url = `https://tasty-treats-backend.p.goit.global/api/recipes/${recipeId}`;
+  
+ return axios
+    .get(url)
+    .then((response) => response.data)
+    .then((recipe) => {
+    displayRecipeVideo(recipe);
+    displayRecipeTitle(recipe);
+    displayRecipeDescription(recipe);
+    displayRecipeTimeCooking(recipe);
+    displayRecipeRating(recipe);
+    displayRecipeHashtags(recipe);
+    displayRecipeIngredients(recipe);
+    displayStarRating(recipe);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+    };
+
+fetchRecipe('6462a8f74c3d0ddd28897fbb').then(recipe => {
+  console.log(recipe);
+ }).catch(error => {
+  console.log(error);
+});
+
+// Відображаю відео на сторінці
+function displayRecipeVideo(recipe) {
+  const recipeVideoIframe = document.querySelector('.recipes-iframe-video');
+  const youtubeLink = recipe.youtube;
+
+  // ID відео з URL YouTube
+  const videoId = getVideoIdFromLink(youtubeLink);
+
+
+  recipeVideoIframe.src = `https://www.youtube.com/embed/${videoId}`;
+}
+
+// Функція для отримання ID відео з URL YouTube
+function getVideoIdFromLink(link) {
+  const regex = /(?:https?:\/\/(?:www\.)?youtube\.com\/watch\?v=|https?:\/\/youtu\.be\/)([\w-]+)/i;
+  const matches = link.match(regex);
+  return matches && matches[1] ? matches[1] : '';
+}
+
+// Функція виведення заголовку
+
+function displayRecipeTitle(recipe) {
+ const recipeTitleEl = document.querySelector('.recipes-title');
+  recipeTitleEl.textContent = recipe.title;
+}
+
+//  Функція виведення опису приготування
+
+function displayRecipeDescription(recipe) {
+ const recipeTitleEl = document.querySelector('.recipes-description');
+  recipeTitleEl.textContent = recipe.instructions;
+}
+
+function displayRecipeTimeCooking(recipe) {
+ const tmeCookingEl = document.querySelector('.recipes-cooking-time');
+ tmeCookingEl.textContent = recipe.time;
+}
+
+function displayRecipeRating(recipe) {
+ const recipeRatingEl = document.querySelector('.ratinng-value');
+ recipeRatingEl.textContent = recipe.rating;
+}
+
+
+
+function displayRecipeHashtags(recipe) {
+  const recipeHashtagsEl = document.querySelector('.recipes-hashtags-list');
+  recipeHashtagsEl.innerHTML = '';
+  recipe.tags.forEach(tag => {
+    const hashtagItemHtml = `<li class="recipes-hashtags-item">#${tag}</li>`;
+    recipeHashtagsEl.insertAdjacentHTML('beforeend', hashtagItemHtml);
+  });
+  
+}
+
+
+
+function displayRecipeIngredients(recipe) {
+  const recipeIngredientsEl = document.querySelector('.recipes-components-list');
+  recipeIngredientsEl.innerHTML = '';
+  recipe.ingredients.forEach(({measure, name})=> {
+    const ingredientsItemHtml = `<li class="recipes-components-item">
+        <p class="recipes-components-item_name">${name}</p>
+        <p class="recipes-components-item_quantity">${measure}</p>
+      </li>`;
+    recipeIngredientsEl.insertAdjacentHTML('beforeend', ingredientsItemHtml);
+  });
+  
+}
+
+
+function displayStarRating(recipe) {
+  const ratingValue = parseFloat(recipe.rating);
+  const starElements = document.querySelectorAll(".modal-rating-star-icon");
+
+  for (let i = 0; i < starElements.length; i++) {
+    if (i < ratingValue) {
+      starElements[i].classList.add("active");
+    } else {
+      starElements[i].classList.remove("active");
+    }
   }
 }
 
-fetchReceipt()
-  .then(data => {
-    if (data) {
-      console.log(data);
-    } else {
-      console.log('Отримання даних не вдалося.');
-    }
-  })
-  .catch(err => {
-    console.error('Сталася помилка:', err);
-  });
-
-
-
-
-
-  
-
-
-
-// Рендеримо розмітку та відображаємо дані з API
-function displayRecipes(recipe) {
-  const { youtube, title, rating, time, ingredients, tags, instructions } = recipe;
-
-  const html = ` <div class="recipes-iframe-wrapper">
-      <iframe
-        class="recipes-iframe-video"
-        src="https://www.youtube.com/embed/${youtube}"
-        frameborder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
-    </div>
-
-    <h2 class="recipes-title">${title}</h2>`
-}
 
 
 
@@ -68,22 +146,8 @@ function displayRecipes(recipe) {
 
 
 
-    // <div class="rating-time-container">
-    //   <p class="ratinng-value">${rating}</p>
-     
-    //   <p class="recipes-cooking-time">${time}</p>
-    // </div>
 
-    // <ul class="recipes-components-list">
-    //   <li class="recipes-components-item">
-    //     <p class="recipes-components-item_name">${ingredients[id]}</p>
-    //     <p class="recipes-components-item_quantity">${ingredients[measure]}</p>
-    //   </li>
-    //   </ul>
-    // <ul class="recipes-hashtags-list">
-    //   <li class="recipes-hashtags-item">${tags}</li>
-     
-    // </ul>
 
-    // <p class="recipes-description">${instructions}
-    // </p>
+
+
+
