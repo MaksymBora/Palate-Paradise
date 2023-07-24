19d48f3853fc612c6c516d2aae687935584fd0e7
MaksymBora <maxboraod@gmail.com>
2023-07-24 14:12:40 +0200

    fix popular

--- src/js/notifications.js
+++ src/js/notifications.js
@@ -5,3 +5,9 @@ export function notifyError(error) {
     timeout: 6000,
   });
 }
+
+export function notifyInfo(error) {
+  Notiflix.Notify.failure('No result for your request, please try again!', {
+    timeout: 6000,
+  });
+}
--- src/js/popular.js
+++ src/js/popular.js
@@ -1,34 +1,38 @@
-import axios from 'axios';
-import Notiflix from 'notiflix';
-
-const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
+import RecipeApiService from './service/service-api';
+import { notifyInfo } from './notifications';
 const popularBox = document.querySelector('.popular');
 
+const recipeApiSeriсe = new RecipeApiService();
 
-getPopular();
+recipeApiSeriсe.getPopular().then(createPopularMarkup).catch(notifyInfo);
 
-async function getPopular() {
-    const url = `${BASE_URL}/recipes/popular`;
-    try {
-        const {data} = await axios.get(url);
-        createPopularMarkup(data);
-        return data;
-    } catch (error) {
-      return Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!', {
-    timeout: 6000,
-  });
-    }
-};
-
-
+// async function getPopular() {
+//   const url = `${BASE_URL}/recipes/popular`;
+//   try {
+//     const { data } = await axios.get(url);
+//     createPopularMarkup(data);
+//     return data;
+//   } catch (error) {
+//     return Notiflix.Notify.failure(
+//       'Oops! Something went wrong! Try reloading the page!',
+//       {
+//         timeout: 6000,
+//       }
+//     );
+//   }
+// }
 
 function createPopularMarkup(data) {
-    const markup = data.map(({ description, preview, title}) => `<ul class="popular-list">
+  const markup = data
+    .map(
+      ({ description, preview, title, _id }) => `<ul class="popular-list">
     <li class="popular-list-item">
       <img src="${preview}" alt="${title}" class="popular-img" loading="lazy" />
             <h3 class="popular-subtitle">${title}</h3>
             <p class="popular-description">${description}</p>
     </li>
-  </ul>`).join('');
-    return popularBox.insertAdjacentHTML('beforeend', markup);
-};
+  </ul>`
+    )
+    .join('');
+  return popularBox.insertAdjacentHTML('beforeend', markup);
+}
