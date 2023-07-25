bc66657599b11700307d362424f8557c8cc42f71
MaksymBora <maxboraod@gmail.com>
2023-07-25 20:08:50 +0200

    fix popular for mob

--- src/js/popular.js
+++ src/js/popular.js
@@ -4,21 +4,50 @@ const popularList = document.querySelector('.popular-list');
 
 const recipeApiSeriсe = new RecipeApiService();
 
-recipeApiSeriсe.getPopular().then(createPopularMarkup).catch(notifyInfo);
-
+recipeApiSeriсe
+  .getPopular()
+  .then(response => {
+    createPopularMarkup(response);
+  })
+  .catch(notifyInfo);
 
 function createPopularMarkup(data) {
-  const markup = data
+  const firstTwoItems = data.slice(0, 2);
+
+  const markup = firstTwoItems
     .map(
       ({ description, preview, title, _id }) => `
-    <li class="popular-list-item" data-id="${_id}">
-      <img src="${preview}" alt="${title}" class="popular-img" loading="lazy" />
-            <div class="popular-list-item-box"><h3 class="popular-subtitle">${title}</h3>
-            <p class="popular-description">${description}</p></div>
-    </li>`
+      <li class="popular-list-item" data-id="${_id}">
+        <img src="${preview}" alt="${title}" class="popular-img" loading="lazy" />
+        <div class="popular-list-item-box">
+          <h3 class="popular-subtitle">${title}</h3>
+          <p class="popular-description">${description}</p>
+        </div>
+      </li>
+    `
     )
     .join('');
-  return popularList.insertAdjacentHTML('beforeend', markup);
-};
 
+  popularList.insertAdjacentHTML('beforeend', markup);
+}
 
+// recipeApiSeriсe
+//   .getPopular()
+//   .then(response => {
+//     createPopularMarkup(response);
+//   })
+//   .catch(notifyInfo);
+
+// function createPopularMarkup(data) {
+//   const markup = data
+//     .map(
+//       ({ description, preview, title, _id }) => `
+//     <li class="popular-list-item" data-id="${_id}">
+//       <img src="${preview}" alt="${title}" class="popular-img" loading="lazy" />
+//             <div class="popular-list-item-box"><h3 class="popular-subtitle">${title}</h3>
+//             <p class="popular-description">${description}</p></div>
+//     </li>`
+//     )
+//     .join('');
+//   return popularList.insertAdjacentHTML('beforeend', markup);
+// }
