54da57d3dbd44c997813371a755143713c9b3c49
MaksymBora <maxboraod@gmail.com>
2023-07-26 00:58:17 +0200

    update rec modal

--- src/js/modals/modal-recipes.js
+++ src/js/modals/modal-recipes.js
@@ -51,26 +51,27 @@ backdrop.addEventListener('click', event => {
   }
 });
 
-// Слухач на пул рецептів для визначення кліку на кнопку картки
+// // Слухач на пул рецептів для визначення кліку на кнопку картки
 
-const recipesContainer = document.querySelector('.recipes');
-recipesContainer.addEventListener('click', async event => {
-  const seeRecipeBtn = event.target.closest(`.rec-btn-open`);
-  if (!seeRecipeBtn) return;
+// const recipesContainer = document.querySelector('.rec-search-item');
 
-  const recipeId = seeRecipeBtn.dataset.id;
-  console.log(recipeId);
-  try {
-    const fetchedRecipe = await fetchRecipe(recipeId);
-    if (fetchedRecipe) {
-      recipe = fetchedRecipe;
-      updateFavoriteButtonStatus(recipe);
-      openModal();
-    }
-  } catch (error) {
-    console.log(error);
-  }
-});
+// recipesContainer.addEventListener('click', async event => {
+//   const seeRecipeBtn = event.target.closest(`.rec-btn-open`);
+//   if (!seeRecipeBtn) return;
+
+//   const recipeId = seeRecipeBtn.dataset.id;
+//   console.log(recipeId);
+//   try {
+//     const fetchedRecipe = await fetchRecipe(recipeId);
+//     if (fetchedRecipe) {
+//       const recipe = fetchedRecipe;
+//       updateFavoriteButtonStatus(recipe);
+//       openModal();
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 // Отримую дані про конкретний рецепт з API по ID
 async function fetchRecipe(recipeId) {
