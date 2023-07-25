14e52c2bf6a42f23dc77a7acfb3a31fd1136c554
MaksymBora <maxboraod@gmail.com>
2023-07-25 04:06:49 +0200

    update

--- src/js/cart/cart.js
+++ src/js/cart/cart.js
@@ -1,3 +1,8 @@
+// document.querySelector('.cart').addEventListener('click', () => {
+//   window.location.href =
+//     'https://maksymbora.github.io/Palate-Paradise/cart.html';
+// });
+
 document.querySelector('.cart').addEventListener('click', () => {
   window.location.href = 'cart.html';
 });
