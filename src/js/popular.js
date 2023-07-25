8bcf0778a8c548174f1642aeccef2ca310d31d92
MaksymBora <maxboraod@gmail.com>
2023-07-25 20:54:48 +0200

    fix popular

--- src/js/popular.js
+++ src/js/popular.js
@@ -1,17 +1,29 @@
 import RecipeApiService from './service/service-api';
 import { notifyInfo } from './notifications';
-const popularList = document.querySelector('.popular-list');
 
 const recipeApiSeriсe = new RecipeApiService();
 
-recipeApiSeriсe
-  .getPopular()
-  .then(response => {
-    createPopularMarkup(response);
-  })
-  .catch(notifyInfo);
+const popularList = document.querySelector('.popular-list');
 
-function createPopularMarkup(data) {
+function countRec() {
+  return window.innerWidth < 768
+    ? recipeApiSeriсe
+        .getPopular()
+        .then(response => {
+          createPopularMarkupMobile(response);
+        })
+        .catch(notifyInfo)
+    : recipeApiSeriсe
+        .getPopular()
+        .then(response => {
+          createPopularMarkup(response);
+        })
+        .catch(notifyInfo);
+}
+
+countRec();
+
+function createPopularMarkupMobile(data) {
   const firstTwoItems = data.slice(0, 2);
 
   const markup = firstTwoItems
@@ -31,23 +43,16 @@ function createPopularMarkup(data) {
   popularList.insertAdjacentHTML('beforeend', markup);
 }
 
-// recipeApiSeriсe
-//   .getPopular()
-//   .then(response => {
-//     createPopularMarkup(response);
-//   })
-//   .catch(notifyInfo);
-
-// function createPopularMarkup(data) {
-//   const markup = data
-//     .map(
-//       ({ description, preview, title, _id }) => `
-//     <li class="popular-list-item" data-id="${_id}">
-//       <img src="${preview}" alt="${title}" class="popular-img" loading="lazy" />
-//             <div class="popular-list-item-box"><h3 class="popular-subtitle">${title}</h3>
-//             <p class="popular-description">${description}</p></div>
-//     </li>`
-//     )
-//     .join('');
-//   return popularList.insertAdjacentHTML('beforeend', markup);
-// }
+function createPopularMarkup(data) {
+  const markup = data
+    .map(
+      ({ description, preview, title, _id }) => `
+    <li class="popular-list-item" data-id="${_id}">
+      <img src="${preview}" alt="${title}" class="popular-img" loading="lazy" />
+            <div class="popular-list-item-box"><h3 class="popular-subtitle">${title}</h3>
+            <p class="popular-description">${description}</p></div>
+    </li>`
+    )
+    .join('');
+  return popularList.insertAdjacentHTML('beforeend', markup);
+}
