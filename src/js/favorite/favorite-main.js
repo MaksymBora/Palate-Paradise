744841213f409289ac1b3bc15a0809ba7b93a702
MaksymBora <maxboraod@gmail.com>
2023-07-26 22:45:26 +0200

    fix remove fav rec

--- src/js/favorite/favorite-main.js
+++ src/js/favorite/favorite-main.js
@@ -121,9 +121,9 @@ function handleRecipeButtonEvent({ target }) {
     removeFavorite(currentBtn);
     updateCategoryList(target);
   }
-  if (currentBtn.name === 'details') {
-    console.log('modal here');
-  }
+  // if (currentBtn.name === 'details') {
+  //   console.log('modal here');
+  // }
 }
 
 // Add event listeners to the corresponding elements when the DOM is fully loaded.
