f9f984ee86dec8c8e8dd54fc6e03a71cda985b9f
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-07-26 15:16:58 +0200

    cart-scroll

---
+++ src/js/cart/cart-scroll.js
@@ -0,0 +1,28 @@
+const scrollToTopBtn = document.querySelector('.scroll-to-top');
+const carSection = document.querySelector('.cart-section');
+let prevScrollPos = window.pageYOffset;
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+  scrollToTopBtn.style.display = 'none';
+}
+
+function scrollToTopButtonVisibility() {
+  const windowHeight = window.innerHeight;
+  const carSectionHeight = carSection.offsetHeight;
+  const scrollY = window.scrollY;
+
+  if (scrollY < prevScrollPos || scrollY + windowHeight > carSectionHeight) {
+    scrollToTopBtn.style.display = 'block';
+  } else {
+    scrollToTopBtn.style.display = 'none';
+  }
+
+  prevScrollPos = scrollY;
+}
+
+scrollToTopBtn.addEventListener('click', scrollToTop);
+window.addEventListener('scroll', scrollToTopButtonVisibility);
