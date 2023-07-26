b4a27497e24353d4f7bf10195a03cd123f4e93c8
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-07-26 15:37:03 +0200

    scroll

--- src/js/favorits-scroll.js
+++ src/js/favorits-scroll.js
@@ -1,3 +1,24 @@
+// const scrollToTopBtn = document.querySelector('.scroll-to-top');
+
+// function scrollToTop() {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: 'smooth',
+//   });
+// }
+
+// scrollToTopBtn.addEventListener('click', scrollToTop);
+
+// window.addEventListener('scroll', () => {
+//   if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+//     setTimeout(() => {
+//       scrollToTopBtn.style.display = 'block';
+//     }, 500);
+//   } else {
+//     scrollToTopBtn.style.display = 'none';
+//   }
+// });
+
 const scrollToTopBtn = document.querySelector('.scroll-to-top');
 
 function scrollToTop() {
@@ -9,11 +30,19 @@ function scrollToTop() {
 
 scrollToTopBtn.addEventListener('click', scrollToTop);
 
+let prevScrollPos = window.pageYOffset;
+
 window.addEventListener('scroll', () => {
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-    setTimeout(() => {
-      scrollToTopBtn.style.display = 'block';
-    }, 500);
+  // Перевірка, чи поточне значення scrollY більше попереднього, тобто чи користувач скролить вгору
+  const isScrollingUp = window.scrollY < prevScrollPos;
+  prevScrollPos = window.scrollY;
+
+  // Показати або приховати кнопку в залежності від умови
+  if (
+    window.scrollY > 0 &&
+    (isScrollingUp || window.scrollY > window.innerHeight / 2)
+  ) {
+    scrollToTopBtn.style.display = 'block';
   } else {
     scrollToTopBtn.style.display = 'none';
   }
--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,24 +1,6 @@
-// const scrollToTopBtn = document.querySelector('.scroll-to-top');
-
-// function scrollToTop() {
-//   window.scrollTo({
-//     top: 0,
-//     behavior: 'smooth',
-//   });
-// }
-
-// scrollToTopBtn.addEventListener('click', scrollToTop);
-
-// window.addEventListener('scroll', () => {
-//   if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-//     scrollToTopBtn.style.display = 'block';
-//   } else {
-//     scrollToTopBtn.style.display = 'none';
-//   }
-// });
-
 const scrollToTopBtn = document.querySelector('.scroll-to-top');
 const contentWrapper = document.querySelector('.content-wrapper');
+let prevScrollPos = window.pageYOffset;
 
 function scrollToTop() {
   window.scrollTo({
@@ -27,18 +9,19 @@ function scrollToTop() {
   });
 }
 
-scrollToTopBtn.addEventListener('click', scrollToTop);
-
-window.addEventListener('scroll', () => {
+function toggleScrollToTopButton() {
   const contentRect = contentWrapper.getBoundingClientRect();
   const windowHeight = window.innerHeight;
+  const scrollY = window.pageYOffset;
 
-  // Коли позиція контенту нижче від вікна, з'являється кнопка
-  if (contentRect.bottom <= windowHeight) {
-    setTimeout(() => {
-      scrollToTopBtn.style.display = 'block';
-    }, 500); // 500 мілісекунд (0.5 секунди) затримки
+  if (scrollY < prevScrollPos || contentRect.bottom <= windowHeight) {
+    scrollToTopBtn.style.display = 'block';
   } else {
     scrollToTopBtn.style.display = 'none';
   }
-});
+
+  prevScrollPos = scrollY;
+}
+
+scrollToTopBtn.addEventListener('click', scrollToTop);
+window.addEventListener('scroll', toggleScrollToTopButton);
