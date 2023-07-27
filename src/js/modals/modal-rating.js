b9e4484313650692c8e9924145dc6ca7e0b0edc2
penadine <105946746+penadine@users.noreply.github.com>
2023-07-27 23:11:28 +0300

    patch

--- src/js/modals/modal-rating.js
+++ src/js/modals/modal-rating.js
@@ -1,5 +1,7 @@
 import throttle from 'lodash.throttle';
 import Notiflix from 'notiflix';
+import { currentRecipeId } from './modal-recipes.js';
+
 
 const refs = {
   openModalRatingBtn: document.querySelector('[data-modal-rating-open]'),
@@ -19,19 +21,35 @@ refs.closeModalRatingBtn.addEventListener('click', onToggleModalRating);
 refs.modalRatingForm.addEventListener('change', throttle(onRatingChange, 500));
 refs.modalRatingForm.addEventListener('submit', onFormSubmit);
 
+
 // Open and close modal-rating modal window
 function onToggleModalRating() {
-  refs.modalRating.classList.toggle('is-hidden');
+  refs.modalRating.classList.toggle('is-hidden')
+  if (refs.modalRating.classList.contains('is-hidden')){
+    refs.modalRatingForm.elements.email.value = '';
+    refs.modalRatingForm.elements.rating.value = '';
+    resetStars();
+  };
 }
 
-refs.modalRatingSubmitBtn.disabled = true;
+// Function to reset star colors to their initial state
+function resetStars() {
+  currentRating = 0;
+  refs.ratings.forEach(rateEl => {
+    rateEl.nextElementSibling.classList.remove('is-active');
+  });
+  refs.modalRatingValue.textContent = currentRating;
+}
+
+
+
 onClickPageReload();
 
 function onRatingChange(evt) {
   const { rating, email } = evt.currentTarget.elements;
 
   const ratingFormState = {
-    rating: rating.value,
+    rating: Number(rating.value),
     email: email.value.trim(),
   };
 
@@ -67,20 +85,99 @@ function onClickPageReload() {
     refs.modalRatingSubmitBtn.disabled = true;
   }
 }
+
+
 //Clean localStorage and form inputs after form submit
 function onFormSubmit(evt) {
   evt.preventDefault();
 
-  onSuccessMes();
-  refs.modalRatingSubmitBtn.disabled = true;
-  localStorage.removeItem(RATING_FORM_KEY);
-  // Clear data from form inputs
-  evt.currentTarget.reset();
-}
-//Show notification after form sending
-function onSuccessMes() {
-  Notiflix.Report.success('Thank you for your feedback!', '', 'Ok', {
-    position: 'center-top',
-    titleMaxLength: '100',
-  });
-}
+  const { rating, email } = evt.currentTarget.elements;
+
+  // Validate rating: Rating should be between 1 and 5
+  const ratingValue = Number(rating.value);
+  if (isNaN(ratingValue) || ratingValue < 1 || ratingValue > 5) {
+    Notiflix.Report.failure(
+      'Error',
+      'Please enter a rating between 1 and 5.',
+      'Ok'
+    );
+    return;
+  }
+const emailValue = email.value.trim();
+  // Validate email: Use regular expression for email format
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(emailValue)) {
+    // Invalid email format
+    Notiflix.Report.failure(
+      'Error',
+      'Please enter a valid email address.',
+      'Ok'
+    );
+    return;
+  }
+
+  // Отправка данных рейтинга на сервер по методу PATCH
+  // Вам нужно заменить 'YOUR_API_URL/{id}/rating' на фактический URL вашего бекенда
+  const recipeIdFromModalRecipes = currentRecipeId;
+  const patchUrl = `https://tasty-treats-backend.p.goit.global/api/recipes/${recipeIdFromModalRecipes}/rating`;
+
+  const requestData = {
+    rate: ratingValue, 
+    email: emailValue, 
+  };
+
+  console.log('Data sent to the backend:', requestData); // Add this line to log the data
+  console.log('RecipeId:', currentRecipeId);
+
+  fetch(patchUrl, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(requestData),
+  })
+    .then(response => {
+      if (response.status === 201) {
+        // Successful response from the server
+        return response.json();
+      } else if (response.status === 409) {
+        // Conflict - Provided email already exists
+        return response.json(); // Extract the data from the response
+      } else if (response.status === 400) {
+        // Bad Request - Invalid request body
+        return response.json(); // Extract the data from the response
+      } else {
+        // Other server-side error (5xx) - Internal Server Error
+        throw new Error('Server Error');
+      }
+      
+    })
+    .then(data => {
+      if (data) {
+        // Data will be defined for 409 and 400 statuses
+        console.log('Data received from server:', data);
+        // Show error messages using Notiflix
+        Notiflix.Report.failure('Error', data.message, 'Ok', {
+          position: 'center-top',
+        });
+      } else {
+        // Data will be undefined for 201 status
+        console.log('Data received from server:', data);
+        // Show success message using Notiflix
+        Notiflix.Report.success('Rating submitted successfully!', '', 'Ok', {
+          position: 'center-top',
+          titleMaxLength: '100',
+        });
+      }
+       onToggleModalRating(); 
+    })
+     .catch(error => {
+      console.error('Error:', error);
+      // Show error message using Notiflix
+      Notiflix.Report.failure(
+        'Error',
+        'An error occurred while submitting the rating. Please try again later.',
+        'Ok'
+      );
+      onToggleModalRating(); 
+    })
