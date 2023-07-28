f32c00cd3d214a7d025596ede813e5333cc85786
MaksymBora <maxboraod@gmail.com>
2023-07-28 18:00:26 +0200

    Welcome to Palata-Paradise

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import './js/theme-switch';
 import './js/hero';
 import './js/all-cat';
 import './js/popular';
-
 import './js/modals/modal-rating';
-
 import './js/modals/modal-order';
-
 import './js/modals/modal-recipes';
-
 import './js/search';
 import './js/cart/cart';
 import './js/all-cat/reset';
 import './js/scroll';
 import './js/modals/open-rating';
 import './js/search/pagination-search';
+// test
